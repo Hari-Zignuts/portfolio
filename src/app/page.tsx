c9b47491a233eaf03fa45dfb9564ae4364b07f82
Hari Malam <harim@zignuts.com>
2025-02-27 10:50:45 +0530

    comming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import Image from 'next/image';
+import devImage from "../assets/developer.jpg";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          Comming Soon
-        </main>
+    <div className="flex flex-col items-center justify-center min-h-screen p-4 sm:p-8 md:p-20 font-[family-name:var(--font-geist-sans)] ">
+      <main className="flex flex-col items-center justify-center gap-4 p-6 sm:p-10 md:p-12   rounded-lg ">
+        <Image src={devImage} alt="my image" className="rounded-full w-24 h-24 sm:w-32 sm:h-32 md:w-48 md:h-48" />
+        <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-center">Hari Malam Portfolio</h1>
+        <p className="text-center text-sm sm:text-base md:text-lg border-2 border-blue-500 p-2 rounded-lg animate-pulse">Coming Soon</p>
+      </main>
     </div>
   );
 }
